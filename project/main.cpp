2780c1708f96d84a8bf691e4e9817087f399190a
MaximeElie <maxime.elie@etu.unilim.fr>
2017-09-19 06:55:46 +0200

    Update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -1,22 +1,19 @@
 #include "Bot.h"
 
-
 int main(int argc, char* argv[]) {
-    Coordinator coordinator;
-    coordinator.LoadSettings(argc, argv);
+	Coordinator coordinator;
+	coordinator.LoadSettings(argc, argv);
 
 	Bot bot;
-    coordinator.SetParticipants({
-        CreateParticipant(Race::Protoss, &bot),
-        CreateComputer(Race::Zerg)
-    });
+	coordinator.SetParticipants({
+		CreateParticipant(Race::Protoss, &bot),
+		CreateComputer(Race::Zerg)
+	});
 
 	coordinator.SetRealtime(true);
-
-    coordinator.LaunchStarcraft();
-    coordinator.StartGame(sc2::kMapBelShirVestigeLE);
+	coordinator.LaunchStarcraft();
+	coordinator.StartGame(sc2::kMapBelShirVestigeLE);
 
 	while (coordinator.Update());
-
-    return 0;
+	return 0;
 }
